b75737e76e234bbeaed84794810fb7282d69e460
Dan Brown <fireflip@gmail.com>
2018-10-18 12:35:40 -0500

    Change field order

--- partials/custom_fields.php
+++ partials/custom_fields.php
@@ -34,7 +34,7 @@ if( function_exists('acf_add_local_field_group') ):
 			),
 		),
 	),
-	'menu_order' => 0,
+	'menu_order' => 10,
 	'position' => 'normal',
 	'style' => 'default',
 	'label_placement' => 'top',
